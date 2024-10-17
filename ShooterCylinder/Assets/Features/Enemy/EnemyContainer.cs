aedd6d7a20e533c5f20c95f2067243dc10e2811c
SinaKarimi <Sinakarimi0938@gmaiol.com>
2024-10-17 16:53:33 +0330

    Create enemyContainer

---
+++ ShooterCylinder/Assets/Features/Enemy/EnemyContainer.cs
@@ -0,0 +1,21 @@
+﻿using Features.ConfigProvider;
+using Features.DependencyInjection.Main;
+using Features.Enemy.Config;
+using UnityEngine;
+
+namespace Features.Enemy
+{
+    public class EnemyContainer : MonoBehaviour, IConfig
+    {
+        private EnemyConfig _enemyConfig;
+
+        private void Awake()
+        {
+            var configProviderService = DependencyInjector.Instance.GetDependency<IConfigProviderService>();
+            configProviderService.RegisterConfig(this);
+            _enemyConfig = Resources.Load<EnemyConfig>("Enemy/EnemyConfig");
+        }
+
+        public EnemyConfig EnemyConfig => _enemyConfig;
+    }
+}
