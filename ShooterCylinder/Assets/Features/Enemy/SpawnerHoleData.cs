d52697d876a7d670766a8897a6aab2385c90daec
SinaKarimi <Sinakarimi0938@gmaiol.com>
2024-10-18 02:09:36 +0330

    Create struct for SpawnerHoleData

---
+++ ShooterCylinder/Assets/Features/Enemy/SpawnerHoleData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Features.Enemy
+{
+    [Serializable]
+    public struct SpawnerHoleData
+    {
+        public int spawnRate;
+        public Vector3 position;
+    }
+}
