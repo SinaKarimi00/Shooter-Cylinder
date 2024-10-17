39da3d06911c340ef63f68ac43a73f4928c9358e
SinaKarimi <Sinakarimi0938@gmaiol.com>
2024-10-17 16:11:03 +0330

    Convert player movment class from mono classes to none mono classes

--- ShooterCylinder/Assets/Features/Player/Movement/PlayerMovement.cs
+++ ShooterCylinder/Assets/Features/Player/Movement/PlayerMovement.cs
@@ -1,23 +1,41 @@
-﻿using UnityEngine;
+﻿using Features.ConfigProvider;
+using Features.DependencyInjection.Main;
+using Features.Main.Application;
+using UnityEngine;
 
 namespace Features.Player.Movement
 {
-    public class PlayerMovement : MonoBehaviour
+    public class PlayerMovement : IUpdater, IFixedUpdater
     {
-        [SerializeField] private Rigidbody rb;
-        public float moveSpeed;
+        private readonly Rigidbody _rb;
+        private readonly float _moveSpeed;
         private float _horizontalInput;
         private float _verticalInput;
+        private float _mouseX;
+        private readonly float _mouseSensitivity;
+        private float _xRotation;
         private Vector3 _moveDirection;
+        private readonly PlayerContainer _playerContainer;
 
-        private void Update()
+        public PlayerMovement()
         {
-            MyInput();
+            Cursor.lockState = CursorLockMode.Locked;
+            var configProviderService = DependencyInjector.Instance.GetDependency<IConfigProviderService>();
+            _playerContainer = configProviderService.GetConfig<PlayerContainer>();
+            _rb = _playerContainer.PlayerRigidbody;
+            _moveSpeed = _playerContainer.PlayerConfig.MoveSpeed;
+            _mouseSensitivity = _playerContainer.PlayerConfig.MouseSensitivity;
         }
 
-        private void FixedUpdate()
+        public void Update()
         {
             MovePlayer();
+        }
+
+        public void FixedUpdate()
+        {
+            MyInput();
+            RotatePlayer();
         }
 
 
@@ -25,12 +43,19 @@ namespace Features.Player.Movement
         {
             _horizontalInput = Input.GetAxisRaw("Horizontal");
             _verticalInput = Input.GetAxisRaw("Vertical");
+            _mouseX = Input.GetAxisRaw("Mouse X");
         }
 
         private void MovePlayer()
         {
             _moveDirection = Vector3.forward * _verticalInput + Vector3.right * _horizontalInput;
-            rb.AddForce(_moveDirection.normalized * (moveSpeed * 10f), ForceMode.Force);
+            _rb.AddForce(_moveDirection.normalized * (_moveSpeed * 10f), ForceMode.Force);
+        }
+
+        private void RotatePlayer()
+        {
+            _mouseX *= _mouseSensitivity;
+            _playerContainer.PlayerTransform.Rotate(Vector3.up * _mouseX);
         }
     }
 }
