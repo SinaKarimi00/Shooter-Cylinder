bf9425b5c6aa3b5f1156fa1e91cc64c7e07bceb7
SinaKarimi <Sinakarimi0938@gmaiol.com>
2024-10-17 16:04:15 +0330

    Create IConfigProviderService

---
+++ ShooterCylinder/Assets/Features/ConfigProvider/Application/IConfigProviderService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Features.DependencyInjection.Application;
+
+namespace Features.ConfigProvider
+{
+    public interface IConfigProviderService : IService
+    {
+        public IConfig GetConfig(Type type);
+        public T GetConfig<T>() where T : IConfig;
+        public void RegisterConfig(IConfig config);
+        public void UnRegisterConfigsOfType<T>() where T : IConfig;
+        public void UnRegisterConfig(IConfig config);
+        public void UnRegisterAll();
+    }
+}
