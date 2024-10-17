05ac6c7d9f456e3356590a21140dc1b8cdded8bf
SinaKarimi <Sinakarimi0938@gmaiol.com>
2024-10-17 16:53:06 +0330

    Create enemy script

---
+++ ShooterCylinder/Assets/Features/Enemy/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using Features.ConfigProvider;
+using Features.DependencyInjection.Main;
+using Features.Player;
+using UnityEngine;
+
+namespace Features.Enemy
+{
+    public class Enemy : MonoBehaviour
+    {
+        private PlayerContainer _playerContainer;
+        private EnemyContainer _enemyContainer;
+        private float _moveSpeed;
+
+        private void Start()
+        {
+            var configProviderService = DependencyInjector.Instance.GetDependency<IConfigProviderService>();
+            _playerContainer = configProviderService.GetConfig<PlayerContainer>();
+            _enemyContainer = configProviderService.GetConfig<EnemyContainer>();
+            _moveSpeed = _enemyContainer.EnemyConfig.EnemyMoveSpeed;
+        }
+
+        private void FixedUpdate()
+        {
+            FollowingPlayer();
+        }
+
+        private void FollowingPlayer()
+        {
+            var enemyDirection = _playerContainer.PlayerTransform.position;
+            transform.position = Vector3.MoveTowards(transform.position, enemyDirection, _moveSpeed * Time.deltaTime);
+        }
+    }
+}
