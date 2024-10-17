aa4e816157f75d3e9f47c78e7506dcfa52441e0c
SinaKarimi <Sinakarimi0938@gmaiol.com>
2024-10-18 02:08:44 +0330

    Added configs of enemy to it's container

--- ShooterCylinder/Assets/Features/Enemy/EnemyContainer.cs
+++ ShooterCylinder/Assets/Features/Enemy/EnemyContainer.cs
@@ -8,14 +8,20 @@ namespace Features.Enemy
     public class EnemyContainer : MonoBehaviour, IConfig
     {
         private EnemyConfig _enemyConfig;
+        private EnemySpawnerConfig _enemySpawnerConfig;
+        private Enemy _enemyPrefab;
 
         private void Awake()
         {
             var configProviderService = DependencyInjector.Instance.GetDependency<IConfigProviderService>();
             configProviderService.RegisterConfig(this);
             _enemyConfig = Resources.Load<EnemyConfig>("Enemy/EnemyConfig");
+            _enemySpawnerConfig = Resources.Load<EnemySpawnerConfig>("Enemy/EnemySpawnerConfig");
+            _enemyPrefab = Resources.Load<Enemy>("Enemy/Enemy");
         }
 
         public EnemyConfig EnemyConfig => _enemyConfig;
+        public Enemy EnemyPrefab => _enemyPrefab;
+        public EnemySpawnerConfig EnemySpawnerConfig => _enemySpawnerConfig;
     }
 }
